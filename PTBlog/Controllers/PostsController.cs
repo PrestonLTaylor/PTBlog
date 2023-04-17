900268519afed55abc3da337bc0acaad9b1328c7
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-04-17 15:38:40 +0100

    Implement initial controller for Posts
    The PostsController is the controller for handling all the posts on the
    website. Currently there is no associated view however, the listings
    action will list all the posts that are stored on the database.

---
+++ PTBlog/Controllers/PostsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PTBlog.Data;
+
+namespace PTBlog.Controllers;
+
+[Route("{controller}")]
+public sealed class PostsController : Controller
+{
+    public PostsController(DatabaseContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [Route("")]
+    [Route("Listings")]
+    public IActionResult Listings()
+    {
+        var posts = _dbContext.Posts.ToList();
+        return View(posts);
+    }
+
+    private readonly DatabaseContext _dbContext;
+}
