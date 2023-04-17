39b70dbc013f32326baa4ef0ef0890d3dc690924
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-04-17 14:30:18 +0100

    Add User and Post models to the DbContext as DbSets

--- PTBlog/Data/DbContext.cs
+++ PTBlog/Data/DbContext.cs
@@ -6,4 +6,7 @@ namespace PTBlog.Data;
 public sealed class DatabaseContext : DbContext
 {
     public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) {}
+
+    public DbSet<UserModel> Users { get; set; }
+    public DbSet<PostModel> Posts { get; set; }
 }
