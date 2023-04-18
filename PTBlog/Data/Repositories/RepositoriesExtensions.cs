5a8d874b86c7dc09ac1ad170a7debeb62825ceb7
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-04-18 17:38:44 +0100

    Register IUsersRepository as a transient

--- PTBlog/Data/Repositories/RepositoriesExtensions.cs
+++ PTBlog/Data/Repositories/RepositoriesExtensions.cs
@@ -5,6 +5,7 @@ public static class RepositoriesExtensions
 	static public IServiceCollection AddRepositoriesSerivces(this IServiceCollection services)
 	{
 		services.AddTransient<IPostsRepository, PostsRepository>();
+		services.AddTransient<IUsersRepository, UsersRepository>();
 		return services;
 	}
 }
