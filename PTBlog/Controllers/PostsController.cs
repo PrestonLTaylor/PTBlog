13ccb7866ca56c9f1a5cfc8de9c1477a707cfc08
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-04-18 14:06:41 +0100

    Add a controller method for displaying a specific blog post

--- PTBlog/Controllers/PostsController.cs
+++ PTBlog/Controllers/PostsController.cs
@@ -20,5 +20,22 @@ public sealed class PostsController : Controller
         return View(posts);
     }
 
+	[Route("{id}")]
+	public async Task<IActionResult> Listing(int? id)
+    {
+        if (id is null || _dbContext.Posts is null)
+        {
+            return NotFound();
+        }
+
+        var post = await _dbContext.Posts.FirstOrDefaultAsync(p => p.Id == id);
+        if (post is null)
+        {
+            return NotFound();
+        }
+
+        return View(post);
+    }
+
     private readonly DatabaseContext _dbContext;
 }
