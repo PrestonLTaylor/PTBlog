3e189c87dffd48ff21efd8bd978c05adb7e0f3ce
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-04-18 17:34:58 +0100

    Implement a UsersRepository for querying users
    The UsersRepository will handle all the querying of users when
    implemented into the UsersController.

---
+++ PTBlog/Data/Repositories/IUsersRepository.cs
@@ -0,0 +1,9 @@
+﻿using PTBlog.Models;
+
+namespace PTBlog.Data.Repositories;
+
+public interface IUsersRepository
+{
+	public Task<List<UserModel>> GetUsersAsync();
+	public Task<UserModel?> GetUserByGuidAsync(Guid? guid);
+}
