2f68ba186518990e9b3009545303d509c95657aa
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-04-18 17:45:07 +0100

    Use IUsersRepository with UsersController

--- PTBlog/Controllers/UsersController.cs
+++ PTBlog/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PTBlog.Data;
+using PTBlog.Data.Repositories;
 using PTBlog.Models;
 
 namespace PTBlog.Controllers;
@@ -8,22 +9,22 @@ namespace PTBlog.Controllers;
 [Route("{controller}")]
 public sealed class UsersController : Controller
 {
-	public UsersController(DatabaseContext dbContext)
+	public UsersController(IUsersRepository usersRepository)
 	{
-		_dbContext = dbContext;
+		_usersRepository = usersRepository;
 	}
 
 	[Route("")]
 	public async Task<IActionResult> Users()
 	{
-		var users = await _dbContext.Users.ToListAsync();
+		var users = await _usersRepository.GetUsersAsync();
 		return View(users);
 	}
 
 	[Route("{guid}")]
 	public async Task<IActionResult> Profile(Guid? guid)
 	{
-		var user = await GetUserByGuidAsync(guid);
+		var user = await _usersRepository.GetUserByGuidAsync(guid);
 		if (user is null)
 		{
 			return NotFound();
@@ -32,15 +33,5 @@ public sealed class UsersController : Controller
 		return View(user);
 	}
 
-	private async Task<UserModel?> GetUserByGuidAsync(Guid? guid)
-	{
-		if (guid is null || _dbContext.Users is null)
-		{
-			return null;
-		}
-
-		return await _dbContext.Users.Include(u => u.Posts).FirstOrDefaultAsync(u => u.Id.Equals(guid));
-	}
-
-	private readonly DatabaseContext _dbContext;
+	private readonly IUsersRepository _usersRepository;
 }
