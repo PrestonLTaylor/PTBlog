2dc2c09cd43f2c8472126ea780bcdb88d0b1a874
PrestonLTaylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-04-22 16:14:39 +0100

    Add a default profile picture when a user's fails to load

---
+++ PTBlog/wwwroot/js/DefaultProfilePicture.js
@@ -0,0 +1,9 @@
+﻿let profilePictures = document.querySelectorAll(".profile-picture, .nav-profile-picture");
+profilePictures.forEach(function (element) {
+    element.addEventListener("error", function (event) {
+        event.target.src = "https://i.imgur.com/ei3kdCj.png";
+
+        // Prevents an onerror being called over and over again if the default profile picture breaks
+        event.target.onerror = null;
+    });
+});
