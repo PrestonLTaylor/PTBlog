8f319c5d2b2f59dfc869b03de16387fce099c9b4
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-04-24 18:47:24 +0100

    Implement the ability to preview posts with markdown
    We use ajax to make sure that the markdown we get will be the same as
    when the post is actually displayed. This means we had to use the same
    markdown renderer.

---
+++ PTBlog/wwwroot/js/PostPreview.js
@@ -0,0 +1,37 @@
+﻿async function TogglePreview() {
+	let postPreview = document.querySelector(".post-preview");
+	if (postPreview.style.display === "none") {
+		await UpdatePreviewContent();
+		postPreview.style.display = "block";
+	} else {
+		postPreview.style.display = "none";
+	}
+}
+
+async function UpdatePreviewContent() {
+	let postPreviewTitle = document.querySelector(".post-preview-title");
+	postPreviewTitle.innerHTML = `Preview for ${document.querySelector(".title-input").value}`;
+
+	let postPreviewContent = document.querySelector(".post-preview-content");
+	let content = await RenderMarkdown(document.querySelector(".content-input").value);
+	postPreviewContent.innerHTML = content;
+}
+
+async function RenderMarkdown(rawMarkdown) {
+	let markdown = "";
+	await $.ajax({
+		contentType: "application/json; charset=utf-8",
+		type: "POST",
+		url: '/Markdown/ParseHTMLString',
+		dataType: "json",
+		data: `${JSON.stringify(rawMarkdown)}`,
+		success: function (renderedMarkdown) {
+			markdown = renderedMarkdown.value;
+		},
+		error: function (req, status, error) {
+			markdown = `Error rendering markdown: ${error}`;
+		}
+	});
+
+	return markdown;
+}
