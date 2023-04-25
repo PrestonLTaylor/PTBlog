ff4a4d8018fbb913794631fe3b4e366694fe1c98
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-04-25 20:04:04 +0100

    Implement function for checking if a ClaimsIdentity has the IsAdmin claim

--- PTBlog/Claims/IsAdminClaim.cs
+++ PTBlog/Claims/IsAdminClaim.cs
@@ -1,7 +1,14 @@
-﻿namespace PTBlog.Claims;
+﻿using System.Security.Claims;
+
+namespace PTBlog.Claims;
 
 public static class IsAdminClaim
 {
 	public const string Name = "isAdmin";
 	public const string DefaultValue = "False";
+
+	public static bool IsUserAnAdmin(ClaimsIdentity user)
+	{
+		return user.HasClaim(Name, true.ToString());
+	}
 }
