3ee7f2f4328716d427e1a4b01d53cb317ff016d4
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2023-05-04 23:02:59 +0100

    Refactor out endpoint bodies into seperate functions

--- PTBlog/Endpoints/V1/PostsEndpoints.cs
+++ PTBlog/Endpoints/V1/PostsEndpoints.cs
@@ -3,29 +3,36 @@ using PTBlog.Models;
 
 namespace PTBlog.Endpoints.V1;
 
+public static class PostsEndpoints
+{
+    static public async Task<IResult> GetAll(IPostsRepository repo)
+    {
+        var posts = await repo.GetPostsAsync();
+        // TODO: Use another DTO (so we can return the post id etc)
+        var postDtos = posts.Select(x => new PostDTO(x.Title, x.Content));
+        return Results.Ok(postDtos);
+    }
+
+    static public async Task<IResult> Get(IPostsRepository repo, int postId)
+    {
+        var post = await repo.GetPostByIdAsync(postId);
+        if (post is null)
+        {
+            return Results.NotFound();
+        }
+
+        var postDto = new PostDTO(post.Title, post.Content);
+        return Results.Ok(postDto);
+    }
+}
+
 public static class PostsEndpointsExtensions
 {
     static public WebApplication MapPostsApiEndpoints(this WebApplication app)
     {
-        app.MapGet(APIRoutes.Posts.GetAll, async (IPostsRepository repo) =>
-        {
-            var posts = await repo.GetPostsAsync();
-            // TODO: Use another DTO (so we can return the post id etc)
-            var postDtos = posts.Select(x => new PostDTO(x.Title, x.Content));
-            return Results.Ok(postDtos);
-        });
+        app.MapGet(APIRoutes.Posts.GetAll, PostsEndpoints.GetAll);
 
-        app.MapGet(APIRoutes.Posts.Get, async (IPostsRepository repo, int postId) =>
-        {
-            var post = await repo.GetPostByIdAsync(postId);
-            if (post is null)
-            {
-                return Results.NotFound();
-            }
-
-            var postDto = new PostDTO(post.Title, post.Content);
-            return Results.Ok(postDto);
-        });
+        app.MapGet(APIRoutes.Posts.Get, PostsEndpoints.Get);
 
         return app;
     }
